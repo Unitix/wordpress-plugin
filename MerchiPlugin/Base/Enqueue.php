017d64df7e65ca94b59b805c33e38e283230b675
James Cool <james@newgenstudio.com>
2025-01-07 12:19:07 +1100

    remove empty files "scripts.js"

--- MerchiPlugin/Base/Enqueue.php
+++ MerchiPlugin/Base/Enqueue.php
@@ -16,7 +16,6 @@ class Enqueue extends BaseController {
 
 	public function do_enqueue() {
 		wp_enqueue_style( 'styles', $this->plugin_url . 'assets/merchi_styles.css' );
-		wp_enqueue_script( 'scripts', $this->plugin_url . 'assets/scripts.js' );
 		$mount_point = get_option( 'merchi_mount_point_id' );
 		$css         = ".$mount_point {visibility: hidden;}";
 		wp_add_inline_style( 'styles', $css );
@@ -64,7 +63,6 @@ class Enqueue extends BaseController {
 		];
 
 		wp_enqueue_style( 'styles',  $this->plugin_url . 'assets/merchi_styles_admin.css' );
-		wp_enqueue_script( 'merchi_plugin_val', $this->plugin_url . 'assets/scripts.js' );
 		wp_localize_script( 'merchi_plugin_val', 'merchiObject', $merchi_plugin_object );
 		wp_enqueue_script( 'ajax_script', $this->plugin_url . 'assets/create_merchi_products.js', [ 'jquery' ] );
 		wp_localize_script(
