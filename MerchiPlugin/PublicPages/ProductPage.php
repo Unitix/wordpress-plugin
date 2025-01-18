49242865ff237630d96c9a48b1276939d9f8bfad
Navneet Golwalkar <rinkujdk@gmail.com>
2025-01-18 13:17:14 +0530

    Updated dynamic attributes in product page template.

--- MerchiPlugin/PublicPages/ProductPage.php
+++ MerchiPlugin/PublicPages/ProductPage.php
@@ -22,18 +22,49 @@ class ProductPage extends BaseController {
 		global $product;
 		// SKU used as Merchi ID. We are checking to see if Merchi ID exists. If so fetch Merchi product.
 		if ($product->get_sku() !== '') {
-			$id        = $product->get_sku();
-			$container = 'product-' . strval( $product->get_id() );
-            $redirectUrl = esc_attr( get_option( 'merchi_redirect_url' ) );
-			$redirectUrlQuote = esc_attr( get_option( 'merchi_redirect_url_quote' ) );
+
+		$sync_keys = array(
+			'redirect_after_success_url' => 'redirectAfterSuccessUrl',
+			'redirect_after_quote_success_url' => 'redirectAfterQuoteSuccessUrl',
+			'redirect_with_value' => 'redirectWithValue',
+			'hide_info' => 'hideInfo',
+			'hide_preview' => 'hidePreview',
+			'hide_price' => 'hidePrice',
+			'hide_title' => 'hideTitle',
+			'hide_calculated_price' => 'hideCalculatedPrice',
+			'include_bootstrap' => 'includeBootstrap',
+			'not_include_default_css' => 'notIncludeDefaultCss',
+			'invoice_redirect' => 'invoiceRedirect',
+			'load_theme' => 'loadTheme',
+			'mount_point_id' => 'mountPointId',
+			'single_column' => 'singleColumn',
+			'quote_requested_redirect' => 'quoteRequestedRedirect',
+			'google_api_public_key' => 'googleApiPublicKey',
+			'allow_add_to_cart' => 'allowAddToCart',
+			'hide_drafting' => 'hideDrafting',
+			);
 			
-			// wp_enqueue_script( 'show_merchi_product', $this->plugin_url . 'assets/show_product.js' );
-			// $script_data = [
-			// 	'mountPointId' => $container,
-			// 	'productId'    => $id,
-            //                     'redirectAfterSuccessUrl' => $redirectUrl
-			// ];
-			// wp_localize_script( 'show_merchi_product', 'merchiShowProductScriptOptions', $script_data );
+			$atts = array(
+				'id' => $product->get_meta('merchi_id'),
+				'redirect_after_success_url' => $product->get_meta('redirectAfterSuccessUrl'),
+				'redirect_after_quote_success_url' => $product->get_meta('redirectAfterQuoteSuccessUrl'),
+				'redirect_with_value' => $product->get_meta('redirectWithValue'),
+				'hide_info' => $product->get_meta('hideInfo'),
+				'hide_preview' => $product->get_meta('hidePreview'),
+				'hide_price' =>  $product->get_meta('hidePrice'),
+				'hide_title' => $product->get_meta('hideTitle'),
+				'hide_calculated_price' => $product->get_meta('hideCalculatedPrice'),
+				'include_bootstrap' => $product->get_meta('includeBootstrap'),
+				'not_include_default_css' => $product->get_meta('notIncludeDefaultCss'),
+				'invoice_redirect' => $product->get_meta('invoiceRedirect'),
+				'load_theme' => $product->get_meta('loadTheme'),
+				'mount_point_id' => $product->get_meta('mountPointId'),
+				'single_column' => $product->get_meta('singleColumn'),
+				'quote_requested_redirect' => $product->get_meta('quoteRequestedRedirect'),
+				'google_api_public_key' => $product->get_meta('googleApiPublicKey'),
+				'allow_add_to_cart' => $product->get_meta('allowAddToCart'),
+				'hide_drafting' => $product->get_meta('hideDrafting')
+			);
 
 			if( get_option( 'merchi_staging_mode' ) == 'yes' ) {
 
@@ -43,9 +74,19 @@ class ProductPage extends BaseController {
 				
 				$url = 'https://merchi.co';
 			}
+
+			$src = $url . '/static/product_embed/js/product.embed.js?product=' . $atts['id'];
+
+			foreach( $atts as $key => $atr ){
+					if("id" == $key ) { continue;
+					}
+					if("" == $atr || !$atr ) { continue;
+					}
+					$src .='&'.$sync_keys[$key]."=".$atr;
+			}
 				
-			$content = '<script type="text/javascript" data-name="product-embed" src="' . $url . '/static/product_embed/js/product.embed.js?product=' . $id . '&hidePreview=true&hideTitle=true&hideInfo=true&hidePrice=true&includeBootstrap=true&singleColumn=true&redirectAfterQuoteSuccessUrl=' . $redirectUrlQuote . '&redirectAfterSuccessUrl=' . $redirectUrl . '&redirectWithValue=true"></script>';
-            echo $content;
+			$content = '<script type="text/javascript" data-name="product-embed" src="'.$src.'"></script>';
+      echo $content;
 		} else {
 			echo 'Merchi product not found.';
 		}
