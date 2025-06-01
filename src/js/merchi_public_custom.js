705622df2fc5efcddecf0008911d5e5428331c9f
James Cool <james@newgenstudio.com>
2025-06-02 07:52:09 +1000

    working on taking control of remove cart item

--- src/js/merchi_public_custom.js
+++ src/js/merchi_public_custom.js
@@ -55,15 +55,15 @@ function setCookie(name, value, days) {
 }
 
 async function createCart() {
-  const domainId = scriptData.merchi_domain; // TODO REMOCE THIS WHEN DONE
+  const domainId = scriptData.merchi_domain;
   const domain = new MERCHI.Domain().id(domainId);
   const cart = new MERCHI.Cart().domain(domain);
   return new Promise((resolve, reject) => {
     cart.create(
       (response) => {
         const c = MERCHI.toJson(response);
-        // Set cart cookie here
-        setCookie("cart-" + scriptData.merchi_domain, c.id + "," + c.token, 1);
+        // Set cart cookie here. The cart cookie is a combo od the cart id and the cart token.
+        setCookie("cart-" + scriptData.merchi_domain, `${c.id},${c.token}`, 1);
         localStorage.setItem("MerchiCart", JSON.stringify(c));
         resolve(response);
       },
@@ -212,6 +212,33 @@ jQuery(document).ready(function ($) {
   } else {
     console.error('MERCHI_LOG: initOrSyncCart function not defined.');
   }
+});
+
+jQuery(document).ready(function($) {
+  // Listen for cart item removal
+  $(document.body).on('removed_from_cart', function(event, fragments, cart_hash, $button) {
+      // const cartItemKey = $button.data('cart_item_key');
+      // const cartLength = Object.keys(fragments['div.widget_shopping_cart_content'] || {}).length;
+      
+      // // Make AJAX call to our custom endpoint
+      // $.ajax({
+      //     url: scriptData.ajaxurl,
+      //     type: 'POST',
+      //     data: {
+      //         action: 'cst_cart_item_after_remove',
+      //         item_id: cartItemKey,
+      //         cart_length: cartLength
+      //     },
+      //     success: function(response) {
+      //         console.log('Cart item removed:', response);
+      //         // Handle any additional actions after successful removal
+      //     },
+      //     error: function(xhr, status, error) {
+      //         console.error('Error removing cart item:', error);
+      //     }
+      // });
+      console.log('cart was removed....');
+  });
 });
 
 // Function to show success message
