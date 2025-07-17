bb95687100f1055bce891348ffb3778fbc4f2496
freyaLi321 <166387838+freyaLi321@users.noreply.github.com>
2025-07-17 10:22:12 +1000

    Revert "merge merchi_checkout_init.js into merchi_product_form.js"

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,23 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'production',
   entry: {
     'merchi_sdk': './src/js/merchi_sdk.js',
     'merchi_product_form': './src/js/merchi_product_form.js',
     'create_merchi_products': './src/js/create_merchi_products.js',
     'export_products': './src/js/export_products.js',
+    'merchi_checkout_init': './src/js/merchi_checkout_init.js',
     'merchi_public_custom': './src/js/merchi_public_custom.js',
     'woocommerce_cart_checkout': './src/js/woocommerce_cart_checkout.js',
     'wordpress_merchi_dashboard': './src/js/wordpress_merchi_dashboard.js',
     'order_confirmation': './src/js/components/OrderConfirmation.js'
   },
+  externals: {
+    'merchi_sdk_js': 'MERCHI_INIT.MERCHI_SDK',
+  },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist/js'),
-    clean: true,
-  },
-  performance: {
-    hints: 'warning',
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
-  optimization: {
-    usedExports: true,
-    sideEffects: false,
-    minimize: true,
-    concatenateModules: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-            drop_debugger: true,
-            pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn'],
-            passes: 2,
-          },
-          mangle: true,
-          format: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-    ],
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: 25,
-      minSize: 20000,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-          priority: 10,
-          enforce: true,
-        },
-        react: {
-          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-          name: 'react',
-          chunks: 'all',
-          priority: 20,
-          enforce: true,
-        },
-        common: {
-          name: 'common',
-          minChunks: 2,
-          chunks: 'all',
-          priority: 5,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-    runtimeChunk: 'single',
-    moduleIds: 'deterministic',
-    chunkIds: 'deterministic',
   },
   module: {
     rules: [
@@ -95,16 +36,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            },
-          },
-          'postcss-loader',
-        ],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
@@ -119,10 +51,5 @@ module.exports = {
       'react-dom': path.resolve('./node_modules/react-dom'),
       'react-dom/client': path.resolve('./node_modules/react-dom/client')
     }
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-    }),
-  ],
+  }
 };
